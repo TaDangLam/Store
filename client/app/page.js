06ced6ee313e284b06b5b2027218bcf94b1e374a
DangLam <tadanglam93@gmail.com>
2023-10-28 10:57:58 +0700

    first commit front end

--- client/app/page.js
+++ client/app/page.js
@@ -4,8 +4,20 @@ import axios from "axios";
 import { useEffect, useState } from "react"
 
 export default function Home() {
-  
+  const [category, setCategory] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/api/category').then(result => setCategory(result.data));
+  },[])
+
   return (
-    <>aaa</>
+    <div className="grid">
+      <div className="bg-white">
+        <ul>
+          <li>aa</li>
+        </ul>
+      </div>
+      <div className="bg-green-200">bb</div>
+    </div>
   )
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -12,6 +12,24 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      width: {
+        488: '50rem',
+      },
+      borderRadius: {
+        'none': '0',
+        'sm': '0.125rem',
+        DEFAULT: '0.25rem',
+        DEFAULT: '4px',
+        'md': '0.375rem',
+        'lg': '0.5rem',
+        '2xl': '2rem',
+        'full': '9999px',
+        'large': '12px',
+      },
+      boxShadow: {
+        'sm': '-5px 0px 2px 0 rgb(0 0 0 / 0.08)',
+        'header': '-1px 7px 0px 0px rgba(0,0,0,0.12)',
+      },
     },
   },
   plugins: [],
